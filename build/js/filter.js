5570c2f8b6b6b2f722284f0d7bdb220817a66835
Matthew Fung <matthew.fung@tesco.com>
2018-09-30 18:38:06 +0100

    Adding expand / contract behaviour

--- build/js/filter.js
+++ build/js/filter.js
@@ -1,16 +1,20 @@
-const addClass = (element, theClass) => {
-  let x = element.className;
-  x.indexOf(theClass) == -1
-  ?
-  element.className += ` ${theClass}`
-  :
-  element.className = element.className.replace(/\brotate\b/, " ");
+const addClass = (element, element2, theClass) => {
+  let x = element.className,
+      y = element2.getAttribute('data-show');
+  if (x.indexOf(theClass) == -1 && y == 'false') {
+    element.className += ` ${theClass}`;
+    element2.setAttribute('data-show', true);
+  } else {
+    element.className = element.className.replace(/\brotate\b/, " ");
+    element2.setAttribute('data-show', false);
+  }
 }
 
 const expand = () => {
   let thehandler = document.getElementById('Checklist');
   let theTarget = document.getElementsByClassName('ddl-rndl')[0];
-  thehandler.addEventListener('click', function(){addClass(theTarget, 'rotate')}, false)
+  let thecollapser = document.getElementById('fields');
+  thehandler.addEventListener('click', function(){addClass(theTarget, thecollapser, 'rotate')}, false)
 }
 
 document.addEventListener('DOMContentLoaded', expand());
