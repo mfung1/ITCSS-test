0bbb54ee41fbf31102e4ae407582dff70019b803
Matthew Fung <matthew.fung@tesco.com>
2018-09-30 20:19:12 +0100

    Fix input styling. Add checklist tempalte.

--- build/js/filter.js
+++ build/js/filter.js
@@ -4,6 +4,7 @@ const addClass = (element, element2, theClass) => {
   if (x.indexOf(theClass) == -1 && y == 'false') {
     element.className += ` ${theClass}`;
     element2.setAttribute('data-show', true);
+    console.log(element.getAttribute('aria-expanded'));
   } else {
     element.className = element.className.replace(/\brotate\b/, " ");
     element2.setAttribute('data-show', false);
@@ -11,10 +12,14 @@ const addClass = (element, element2, theClass) => {
 }
 
 const expand = () => {
-  let thehandler = document.getElementById('Checklist');
+  let thehandler = document.getElementById('checklist-btn');
   let theTarget = document.getElementsByClassName('ddl-rndl')[0];
   let thecollapser = document.getElementById('fields');
-  thehandler.addEventListener('click', function(){addClass(theTarget, thecollapser, 'rotate')}, false)
+  thehandler.addEventListener('click', function(){addClass(theTarget, thecollapser, 'rotate')}, false);
+  thehandler.addEventListener('keypress', function (e) {
+    if(e.key == 'Spacebar' || e.key == 'Return'){
+      addClass(theTarget, thecollapser, 'rotate');
+    }}, false);
 }
 
 document.addEventListener('DOMContentLoaded', expand());
