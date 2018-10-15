9ec5e9a8d92b794b92ca7909ed8aa17cfe7aba82
Matthew Fung <matthew.fung@tesco.com>
2018-10-15 15:23:54 +0100

    Updated styles

--- build/js/filter.js
+++ build/js/filter.js
@@ -1,7 +1,7 @@
 // Filter
 // Bind event listeners to inputs
 
-const fields = document.querySelectorAll('[type=checkbox]'),
+const fields = document.querySelectorAll('.checkbox input[type=checkbox]'),
       cards = document.querySelectorAll(".row"),
       apply = document.querySelector("#apply-filter"),
       remove = document.querySelector("#remove-filter"),
@@ -15,33 +15,33 @@ const getEvents = () => {
   elements[i].addEventListener("click",(function(e) {
       if (this.checked) {
         selected.push(e.target.id);
-        console.log(selected);
       } else {
         let index = selected.indexOf(e.target.id);
         selected.splice(index, 1);
-        console.log(selected);
       }
     }));
   }
   addBtnEvents();
 }
 
-const addBtnEvents =  () => {
+const addBtnEvents = () => {
 
     apply.addEventListener('click', function () {
       for (let select in selected) {
         let index = selected[select];
         targets[index].setAttribute('data-visible', 'false');
+        targets[index].setAttribute("style", "margin: 0!important");
       }
     });
     remove.addEventListener('click', function () {
       for (let select in selected) {
         let index = selected[select];
         targets[index].setAttribute('data-visible', 'true');
-        elements[select].checked = false;
+        elements[index].checked = false;
+        targets[index].setAttribute("style", "");
       }
       selected = [];
     });
 }
 
-document.addEventListener('DOMContentLoaded', getEvents());
+document.addEventListener('DOMContentLoaded', getEvents);
