3b773b924a032d8f13add53e1bd31ad1da9c485e
Matthew Fung <matthew.fung@tesco.com>
2018-10-16 23:56:42 +0100

    Update spacings and fix js

--- build/js/filter.js
+++ build/js/filter.js
@@ -1,8 +1,9 @@
 // Filter
 // Bind event listeners to inputs
 
-const fields = document.querySelectorAll('.checkbox input[type=checkbox]'),
+const fields = document.querySelectorAll('.form_field'),
       cards = document.querySelectorAll(".row"),
+      inputs = document.querySelectorAll('input[type="checkbox"]'),
       apply = document.querySelector("#apply-filter"),
       remove = document.querySelector("#remove-filter"),
       targets = [...cards],
@@ -13,13 +14,27 @@ let selected = [];
 const getEvents = () => {
   for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click",(function(e) {
-      if (this.checked) {
+      if (inputs[i].checked == false) {
         selected.push(e.target.id);
+        inputs[i].checked = true;
       } else {
         let index = selected.indexOf(e.target.id);
         selected.splice(index, 1);
+        inputs[i].checked = false;
       }
     }));
+  elements[i].addEventListener("keypress",(function(e) {
+    if(e.keyCode == 32 || e.keyCode == 13 || e.charCode == 32 || e.charCode == 13) {
+      if (inputs[i].checked == false) {
+        selected.push(e.target.id);
+        inputs[i].checked = true;
+      } else {
+        let index = selected.indexOf(e.target.id);
+        selected.splice(index, 1);
+        inputs[i].checked = false;
+      }
+    }
+  }));
   }
   addBtnEvents();
 }
@@ -46,13 +61,13 @@ const addBtnEvents = () => {
 
     apply.addEventListener('click', hideSections);
     apply.addEventListener('keypress', function (e) {
-      if(e.key == 'Spacebar' || e.key == 'Return'){
+      if(e.keyCode == 32 || e.keyCode == 13 || e.charCode == 32 || e.charCode == 13){
         hideSections();
       }}, false);
     apply.addEventListener('touch', hideSections);
     remove.addEventListener('click', showSections);
     remove.addEventListener('keypress', function (e) {
-      if(e.key == 'Spacebar' || e.key == 'Return'){
+      if(e.keyCode == 32 || e.keyCode == 32 || e.charCode == 32 || e.charCode == 13){
         showSections();
       }}, false);
     remove.addEventListener('touch', showSections);
